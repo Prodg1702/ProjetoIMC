ca204e25fb1cd2ca66065fa34875ae07ebb2bd18
Prodg1702 <prodgcoelho88@gmail.com>
2022-10-28 11:22:57 -0300

    imc personalizado e com salvamento de dados

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Attribute, Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,52 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  altura: string
+  peso: string
+  sexo: string
+  shape: string
+  saida: string
+
+
+  
   constructor() {}
 
+  imc() {
+    var gen = this.sexo
+
+    var A1 = parseFloat(this.altura)
+    var P1 = parseFloat(this.peso)
+
+    var resultado = P1 / (A1 **2)
+
+    this.saida = resultado.toString()
+
+    if (gen == "fem"){
+      if (resultado <= 19){
+        this.shape = 'Magro pa Garai'
+      }
+      if (resultado > 19 && resultado <=27.3){
+        this.shape = 'Normal'
+      }
+      if (resultado > 27.3){
+        this.shape = 'Planeta'
+      }
+    }
+
+    if (gen == "masc"){
+      if (resultado <=20.7){
+        this.shape = 'Filé de borboleta'
+      }
+      if (resultado > 20.7 && resultado <= 27.8){
+        this.shape = 'Normal'
+      }
+      if (resultado > 27.8){
+        this.shape = 'SOL andando na terra'
+      }
+    }
+
+  }
+
 }
+
+
